84e5eaec92fd5d6b55153111bc8dab81a5320b86
SAMYAK99 <singhsamyak199@gmail.com>
2022-02-15 00:48:41 +0530

    Initial commit

---
+++ app/src/main/java/com/projects/editit/Result_Actvity.java
@@ -0,0 +1,14 @@
+package com.projects.editit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Result_Actvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result__actvity);
+    }
+}
