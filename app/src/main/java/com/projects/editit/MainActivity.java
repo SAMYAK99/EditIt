916d7558ee83866ca89f6ac151c533911b21848c
SAMYAK99 <singhsamyak199@gmail.com>
2022-02-15 00:59:31 +0530

    Added View Binding

--- app/src/main/java/com/projects/editit/MainActivity.java
+++ app/src/main/java/com/projects/editit/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.projects.editit.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+     ActivityMainBinding mainBinding ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // View Binding :
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+
+
     }
 }
